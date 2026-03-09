461fa4b115c122d2124b5a3635a2d434111e1afc
Janrenz Macuha <janrenzmacuha@Janrenzs-MacBook-Air.local>
2026-03-10 07:06:29 +0800

    Intialize

--- front-page.php
+++ front-page.php
@@ -9,8 +9,18 @@
                     <p><?php eco the_field("description") ?></p>
                     <a class="btn bg--primary" href="#">EXPLORE MORE</a>
                 </div>
+
+                    <?php
+
+                    $banner_image_1 = get_field
+                    ("banner_image_1");
+                    $banner_image_2 = get_field
+                    ("banner_image_2");
+
+                    ?>
+
                 <div class="banner__img">
-                    <img src="./img/Hero.png" alt="banner-img">
+                    <img src="<?php echo esc_url(wp_get_attachment_image_url) ?>" alt="banner-img">
                 </div>
             </div>
         </div>
